f3ceebf6dbc80b1d7fe3cd32d90a7eeba6878b1d
Karl Krueger <karl@udacity.com>
2017-08-03 14:34:48 -0700

    Create testfoo.py

---
+++ testfoo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import code, io, contextlib
+
+class TestConsole(code.InteractiveConsole):
+  def test(self, output):
+    if output.startswith("17"):
+      print("*** YOU WIN A THE INTERNET ***")
+    else:
+      print("--- Hint: try printing smallish primes ---")
+
+  def runcode(self, code):
+    buf = io.StringIO()
+    with contextlib.redirect_stdout(buf):
+      super(TestConsole, self).runcode(code)
+    print(buf.getvalue())
+    self.test(buf.getvalue())
+
+
+if __name__ == '__main__':
+  t = TestConsole()
+  t.interact()
