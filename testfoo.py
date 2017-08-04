3a1ea99040269eefb1dc2ad5688a9e2f653e178a
loaner8 <loaner8@loaner8.local>
2017-08-04 11:43:23 -0700

    Reverse video and input testing

--- testfoo.py
+++ testfoo.py
@@ -2,21 +2,32 @@
 
 import code, io, contextlib, readline
 
+try:
+    import termcolor
+    def fancyprint(s):
+        termcolor.cprint(s, attrs=['reverse'])
+except ImportError:
+    fancyprint = print
+
+
 class TestConsole(code.InteractiveConsole):
-  def test(self, output):
-    if output.startswith("17"):
-      print("*** YOU WIN A THE INTERNET ***")
-    else:
-      print("--- Hint: try printing smallish primes ---")
+  def test_input(self, lines):
+      if lines[0].startswith('def '):
+          fancyprint('You just defined a function!')
+
+  def test_output(self, output):
+      if output.startswith("17"):
+          fancyprint("*** YOU WIN A THE INTERNET ***")
 
   def runcode(self, code):
-    buf = io.StringIO()
-    with contextlib.redirect_stdout(buf):
-      super(TestConsole, self).runcode(code)
-    print(buf.getvalue())
-    self.test(buf.getvalue())
+      self.test_input(self.buffer)
+      buf = io.StringIO()
+      with contextlib.redirect_stdout(buf):
+          super(TestConsole, self).runcode(code)
+      print(buf.getvalue())
+      self.test_output(buf.getvalue())
 
 
 if __name__ == '__main__':
-  t = TestConsole()
-  t.interact()
+    t = TestConsole()
+    t.interact()
